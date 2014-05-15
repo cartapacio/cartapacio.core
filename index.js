e03f93adc764ee637aad83d6c9ab80bd93801f27
Gabriel Zea <zea@randomlab.net>
2014-05-15 16:59:24 -0500

    module wrapper

--- index.js
+++ index.js
@@ -1,6 +1,121 @@
 'use strict';
 
-var Template = require('./src/Template')
+var async = require('async'),
+  path = require('path'),
+  _ = require('lodash'),
+  Template = require('./src/template'),
+  Loader = require('./src/load'),
+  Writer = require('./src/write')
 
-var templates = new Template('op1', 'op2', 'op3')
+/*
+  params:
+    tplPath: rootFile for templates
+    output: output dir
+*/
 
+function Cartapacio (tplPath, output, callback) {
+  this.paths = {
+    templatesRoot: tplPath,
+    partials: path.join(tplPath, 'src', 'templates', 'partials'),
+    layouts:  path.join(tplPath, 'src', 'templates', 'layouts'),
+    output: output
+  }
+
+  this.callback = callback
+
+  this.init()
+}
+
+Cartapacio.prototype.init = function() {
+  this.templates = new Template()
+
+  this.partials = new Loader(this.paths.partials)
+  this.layouts = new Loader(this.paths.layouts)
+
+  this.writer = new Writer(this.paths.output)
+
+  this.loadNcompile()
+};
+
+Cartapacio.prototype.loadNcompile = function() {
+  var self = this
+
+  async.waterfall(
+    [
+      function (next){
+        self.partials.read(function (err, data){
+          if (err) {
+            return next('Reading partials:' + err, null)
+          }
+
+          _.each(data, function (file){
+            self.templates.registerPartial(file.name, file.content)
+          })
+
+          next(null)
+        })
+      },
+
+      function (next){
+        self.layouts.read(function (err, data){
+          if (err) {
+            return next('Reading layouts: ' + err, null)
+          }
+
+          next(null, data)
+        })
+      },
+
+      function (files, next){
+        self.templates.compile(files[0].content, function (err, tpl){
+          if (err){
+            return next('Compiling template: ' + err, null)
+          }
+
+          next(null, tpl)
+        })
+      }
+    ],
+    function (err, tpl){
+      if (err){
+        self.callback(err)
+      }
+
+      self.template = tpl
+      self.callback(null)
+    }
+  )
+}
+
+Cartapacio.prototype.write = function(input, callback) {
+  var self = this
+
+  async.waterfall([
+    function (next){
+      self.templates.render(self.template, input, function (err, data){
+        if (err) {
+          return next('Rendering: ' + err, null)
+        }
+        next(null, data)
+      })
+    },
+    function (data, next){
+      self.writer.create(input.site.filename, data, function(err){
+        if (err) {
+          next(err, null)
+        }
+        next(null, 'File: ' + input.site.filename + ' written')
+      })
+      }
+    ],
+    function (err, result){
+      if (err){
+        callback(err, null)
+      }
+      callback(null, result)
+    }
+  )
+};
+
+
+module.exports = Cartapacio
