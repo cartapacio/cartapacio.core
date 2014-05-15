70f6b72aec1168a6ad57a0bcab4c753f549d9a15
Gabriel Zea <zea@randomlab.net>
2014-05-15 15:27:48 -0500

    load and compile templates

--- src/template.js
+++ src/template.js
@@ -1,14 +1,46 @@
 'use strict';
 
 var handlebars = require('handlebars'),
-  helpers = require('handlebars-helpers')
+  helpers = require('handlebars-helpers').register(handlebars, {}),
+  chalk = require('chalk')
 
-function Template (layouts, pages, partials) {
-  this.layouts = layouts
-  this.pages = pages
-  this.partial = partials
+function Template () {
+  // this.layouts = layouts
+  // this.pages = pages
+  // this.partial = partials
 }
 
+Template.prototype.compile = function(template, callback) {
+  var tpl
 
+  try {
+    tpl = handlebars.compile(template)
+  } catch (ex){
+    callback(ex, null)
+  }
+
+  callback(null, tpl)
+}
+
+Template.prototype.render = function(template, data, callback) {
+  var output
+
+  try {
+    output = template(data)
+  } catch (ex){
+    callback(ex, null)
+  }
+
+  callback(null, output)
+}
+
+Template.prototype.registerPartial = function(name, content) {
+  try {
+    handlebars.registerPartial(name, content)
+  } catch (ex){
+    console.log( chalk.red(ex) )
+  }
+
+}
 
 module.exports = Template
